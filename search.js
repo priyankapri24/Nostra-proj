b350d2ff56fe5708686d08026cf9e036f402ae39
priyankapri24 <priyankadevib006@gmail.com>
2024-06-17 16:12:47 +0530

    updated search responsive

--- search.js
+++ search.js
@@ -1,22 +1,18 @@
-// seearchfunct
+// search
+
 var search=document.getElementById('search')
-var procontainer=document.querySelector('.procontainer')
-var h3list=procontainer.querySelector('div')
+var products = document.querySelector(".products")
+var items =products.querySelectorAll(".item")
+
+search.addEventListener("keyup",()=>{
+    var enteredValue = event.target.value.toUpperCase()
+    for(count=0; count<items.length;count++) {
+        if(items[count].textContent.toUpperCase().indexOf(enteredValue)<0){
+            items[count].style.display = "none";
+        } else {
+            items[count].style.display = "block";
+        }
+    }
+})
 
 
-search.addEventListener('keyup',function(){
-    var enteredvalue = event.target.value.toUpperCase()
-    console.log(enteredvalue)
-    for(count=0;count<h3list;count=count+1)
-    {
-        var h3list=h3list[count].querySelector('h3').textContent
-
-         if(h3list.toUpperCase().indexOf(enteredvalue)<0)
-         {
-             h3list[count].style.display='none'
-         }
-         else{
-            h3list[count].style.display='block'
-         }
-    }       
-    })
